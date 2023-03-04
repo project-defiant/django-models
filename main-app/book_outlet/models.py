9cc5521a06ef4ba6f5d5f5c1f39f2c1db3c6de81
PROJECT-DEFIANT <szymonszyszkowski@gmail.com>
2023-03-04 20:16:01 +0100

    slug in model

--- main-app/book_outlet/models.py
+++ main-app/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxLengthValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -12,9 +13,14 @@ class Book(models.Model):
     )
     author = models.CharField(max_length=400, null=True)
     is_best_selling_book = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.id])
 
     def __str__(self):
         return f"{self.title}-{self.rating}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
